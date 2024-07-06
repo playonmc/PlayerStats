c9901da9f8fd8698fdbd85d409f36ae423a22a6e
Charlie Joseph <ch@rli.es>
2024-07-06 22:29:57 +0100

    Add handling for onDisable

--- src/main/java/mc/play/stats/PlayerStatsPlugin.java
+++ src/main/java/mc/play/stats/PlayerStatsPlugin.java
@@ -4,7 +4,9 @@ import com.google.common.collect.Lists;
 import mc.play.stats.http.SDK;
 import mc.play.stats.listener.*;
 import mc.play.stats.obj.Event;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,13 +15,16 @@ import java.util.List;
 public class PlayerStatsPlugin extends JavaPlugin {
     private List<Event> events;
     private SDK sdk;
+    private BukkitTask task;
 
     @Override
     public void onEnable() {
-        events = new ArrayList<>();
+        if(events == null) {
+            events = new ArrayList<>();
+        }
         sdk = new SDK("TO-BE-CHANGED");
 
-        getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+        task = getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
             List<Event> runEvents = Lists.newArrayList(events.subList(0, Math.min(events.size(), 250)));
             if (runEvents.isEmpty()) return;
 
@@ -57,6 +62,18 @@ public class PlayerStatsPlugin extends JavaPlugin {
                 new RespawnListener(this),
                 new ShearListener(this)
         ).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
+    }
+
+    @Override
+    public void onDisable() {
+        task.cancel();
+    }
+
+    public void triggerEvent(Event event, Player player) {
+        event.setMetadata("playerName", player.getName());
+        event.setMetadata("playerUuid", player.getUniqueId().toString());
+
+        addEvent(event);
     }
 
     public void addEvent(Event event) {
